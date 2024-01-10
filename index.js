55de8720744b3a9d707df5aed811c30270a894e6
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2024-01-10 13:32:12 +0600

    edit

--- index.js
+++ index.js
@@ -31,8 +31,18 @@ async function run() {
     const cartCollection = client.db("bistroBossDB").collection("carts");
 
     // user collection apis
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
     app.post("/users", async (req, res) => {
       const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exists" });
+      }
       const result = await usersCollection.insertOne(user);
       res.send(result);
     });
